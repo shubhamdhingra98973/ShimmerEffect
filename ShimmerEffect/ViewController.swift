de3dddfde15fedc0e1779ed79d94691269758383
Ranu Bhai <ranufrancis@shubhams-mac-mini.local>
2018-12-06 10:31:22 +0530

    Initial Commit

---
+++ ShimmerEffect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShimmerEffect
+//
+//  Created by Shubham on 06/12/18.
+//  Copyright © 2018 Franciscan Ecare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
